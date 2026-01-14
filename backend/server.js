d46b43e960e72c162265e305e107504c71d527f6
Kaustubh Hiwanj <kaustubhhiwanj@Kaustubhs-MacBook-Pro-2.local>
2026-01-15 02:11:59 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "http://localhost:3001",
+    "http://localhost:3001","https://openbazar.kaustubh.codes/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
